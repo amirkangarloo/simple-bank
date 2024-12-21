30e7c823bf1806efa1c89bf9b2a9c5aec8a5b822
Amir Kangarloo <amirkangarloo1051@gmail.com>
2024-12-22 00:46:59 +0330

    feat(AccountTest): Complete unit test for account functions

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -2,13 +2,15 @@ package db
 
 import (
 	"context"
+	"database/sql"
 	"simple-bank/util"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 )
 
-func TestCreateAccount(t *testing.T) {
+func createRandomAccount(t *testing.T) Account {
 	arg := CreateAccountParams{
 		Owner:    util.RandomOwner(),
 		Balance:  util.RandomMoney(),
@@ -26,4 +28,67 @@ func TestCreateAccount(t *testing.T) {
 
 	require.NotZero(t, account.ID)
 	require.NotZero(t, account.CreatedAt)
+
+	return account
+}
+
+func TestCreateAccount(t *testing.T) {
+	createRandomAccount(t)
+}
+
+func TestDeleteAccount(t *testing.T) {
+	account := createRandomAccount(t)
+	err := testQueries.DeleteAccount(context.Background(), account.ID)
+	require.NoError(t, err)
+
+	fetchAccount, err := testQueries.GetAccount(context.Background(), account.ID)
+	require.Error(t, err)
+	require.EqualError(t, err, sql.ErrNoRows.Error())
+	require.Empty(t, fetchAccount)
+}
+
+func TestGetAccount(t *testing.T) {
+	account := createRandomAccount(t)
+	fetchAccount, err := testQueries.GetAccount(context.Background(), account.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, fetchAccount)
+
+	require.Equal(t, account.ID, fetchAccount.ID)
+	require.Equal(t, account.Owner, fetchAccount.Owner)
+	require.Equal(t, account.Balance, fetchAccount.Balance)
+	require.Equal(t, account.Currency, fetchAccount.Currency)
+	require.WithinDuration(t, account.CreatedAt, fetchAccount.CreatedAt, time.Second)
+}
+
+func TestListAccounts(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		createRandomAccount(t)
+	}
+
+	arg := ListAccountsParams{Limit: 5, Offset: 5}
+
+	accounts, err := testQueries.ListAccounts(context.Background(), arg)
+	require.NoError(t, err)
+	require.Len(t, accounts, 5)
+
+	for _, account := range accounts {
+		require.NotEmpty(t, account)
+	}
+}
+
+func TestUpdateAccount(t *testing.T) {
+	account := createRandomAccount(t)
+	arg := UpdateAccountParams{
+		ID:      account.ID,
+		Balance: util.RandomMoney(),
+	}
+	fetchAccount, err := testQueries.UpdateAccount(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, fetchAccount)
+
+	require.Equal(t, account.ID, fetchAccount.ID)
+	require.Equal(t, arg.Balance, fetchAccount.Balance)
+	require.Equal(t, account.Owner, fetchAccount.Owner)
+	require.Equal(t, account.Currency, fetchAccount.Currency)
+	require.WithinDuration(t, account.CreatedAt, fetchAccount.CreatedAt, time.Second)
 }
